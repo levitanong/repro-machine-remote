a44af7ac40948ba087ea05407b778c57de5be9d5
Levi Tan Ong <levi.ong@gmail.com>
2018-11-22 14:35:47 +0800

    minimal repro

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -4,25 +4,57 @@
    [com.wsscode.pathom.core :as p]
    [com.wsscode.pathom.connect :as pc :refer [defresolver defmutation]]
    [com.wsscode.pathom.fulcro.network :as pn]
+   [devcards.core :as dc]
    [fulcro.client :as fc]
+   [fulcro.client.cards :refer [defcard-fulcro]]
    [fulcro.client.dom :as dom]
-   [fulcro.client.primitives :as prim :refer [defsc]])
+   [fulcro.client.primitives :as prim :refer [defsc]]
+   [fulcro.incubator.ui-state-machines :as uism :refer [defstatemachine]])
   (:require-macros
    [clojure.core.async :refer [go]]))
 
 (defonce app (atom nil))
 
+(defstatemachine foo-machine
+  {::uism/actor-names #{:foo :root}
+   ::uism/states
+   {:initial {::uism/events {::uism/started {::uism/target-state :default}}}
+    :default {::uism/events
+              {:exit              {::uism/target-state ::uism/exit}
+               :do-stuff          {::uism/handler (fn [env]
+                                                    (-> env
+                                                        (uism/trigger-remote-mutation
+                                                         :root
+                                                         `some-mutation
+                                                         {::uism/mutation-remote :custom
+                                                          ::uism/ok-event        :mutation-happened})))}
+               :mutation-happened {::uism/handler (fn [env]
+                                                    (js/console.log "mutation happened" env))}}}}})
+
 (defsc Root
   [this _]
-  {}
-  (dom/div nil "hi"))
+  {:ident                (fn [] [:root/by-id 0])
+   :query                [:derp]
+   :initial-state        {:derp 0}
+   :componentDidMount    (fn []
+                           (uism/begin! this foo-machine ::foo {:root (uism/with-actor-class
+                                                                        [:root/by-id 0]
+                                                                        Root)}))
+   :componentWillUnmount (fn []
+                           (uism/trigger! this ::foo :exit))}
+  (dom/div
+    (dom/button {:onClick (fn []
+                            (uism/trigger! this ::foo :do-stuff))}
+      "Derp")))
 
 (pc/defmutation some-remote-mutation
   [env params]
   {::pc/sym    `some-mutation
    ::pc/params []}
   (go
+    (js/console.log "remote-mutation before-timeout")
     (<! (timeout 1000))
+    (js/console.log "remote-mutation after-timeout")
     nil))
 
 (def registry
@@ -49,21 +81,9 @@
   (pn/pathom-remote
    (rest-parser app {})))
 
-(defn started-callback
-  [{:keys [reconciler] :as app}]
-  (js/console.log "started!")
-  )
+(defcard-fulcro derpes
+  Root
+  {}
+  {:fulcro {:networking {:custom (new-custom-remote app)}}})
 
-(defn start []
-  (swap! app
-         (fn [application]
-           (fc/mount application Root "app"))))
-
-(defn init ^:export
-  []
-  (reset!
-   app
-   (fc/new-fulcro-client
-    :networking {:custom (new-custom-remote app)}
-    :started-callback started-callback))
-  (start))
+(defn init ^:export [] (dc/start-devcard-ui!))
